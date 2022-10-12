9c9cc14d422d275f805fa1b7dab67d2536f12f7a
aarón montoya-moraga <montoyamoraga@gmail.com>
2022-10-12 18:39:51 -0300

    subir nuevos ejemplos

--- clases/clase-10/codigo/ej-02-tiempo/code.py
+++ clases/clase-10/codigo/ej-02-tiempo/code.py
@@ -1,10 +1,13 @@
-# ej-01-while-true
+# ej-02-tiempo
 # por montoyamoraga, ipassala
 # para AUDIV020 Diseno de instrumentos musicales digitales
 # v0.0.1 octubre 2022
 # hecho con Mu Editor 1.1.1
 
 # haz click en "Serial" para ver el resultado
+
+# importar biblioteca para tiempo
+import time
 
 # crear variable x
 # darle valor inicial 0
@@ -18,7 +21,10 @@ while True:
 
     # imprime x
     print(x)
-    
+
     # actualiza el valor de x
     # nuevo valor = valor actual + 1
     x = x + 1
+
+    # espera 1.0 segundos
+    time.sleep(1.0)
